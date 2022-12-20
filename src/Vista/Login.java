471ab0cb65fc90b8d267124c5d79e91b1c013811
jeandiegoconi <86448605+jeandiegoconi@users.noreply.github.com>
2022-12-20 18:56:30 -0300

    ahora el login aparece en el centro Bv

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -151,6 +151,7 @@ public class Login extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
